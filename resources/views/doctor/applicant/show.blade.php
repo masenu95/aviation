ca81beee5cffb5c76f2c6e8f89d09814e6a8c9e1
masenu95 <51070735+masenu95@users.noreply.github.com>
2020-08-23 05:24:17 -0700

    appointment
    cghcgshasd

--- resources/views/doctor/applicant/show.blade.php
+++ resources/views/doctor/applicant/show.blade.php
@@ -513,7 +513,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary">Confirm appointment</button>
                 </div>
             </form>
         </div>
